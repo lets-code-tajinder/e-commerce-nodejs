43dfe9e51f9f465c033b8588b64a10ccd715d4a7
Tajinder Singh <101112259+lets-code-tajinder@users.noreply.github.com>
2024-06-10 16:16:51 +0530

    create route for add products

--- models/Product.ts
+++ models/Product.ts
@@ -1,20 +1,28 @@
 import mongoose, { Document, Schema } from "mongoose";
 
 export interface IProduct extends Document {
+  categoryId: number;
   productName: string;
-  price: number;
-  description: string;
+  productDescription: string;
+  productPrice: number;
+  productImage: string;
+  displayOrder: string;
+  status: string;
+  productQuantity: number;
   special: boolean;
   id: number;
-  categoryId: number;
 }
 
 const ProductSchema: Schema = new Schema({
   categoryId: { type: Number, required: true },
+  productName: { type: String, required: true, unique: true },
+  productDescription: { type: String, required: true },
+  productPrice: { type: String, required: true },
+  productImage: { type: String, required: true },
+  displayOrder: { type: String },
+  status: { type: String },
+  productQuantity: { type: Number, required: true },
   id: { type: Number, required: true },
-  productName: { type: String, required: true },
-  price: { type: Number, required: true },
-  description: { type: String, required: true },
   special: { type: Boolean, default: false },
 });
 
